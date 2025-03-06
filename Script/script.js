cb71e0b305fa67161a9f1f6d78ea87d0fcea3281
Victor Fineto <victorfinetosilva@uol.com.br>
2025-03-06 20:21:30 -0300

    inicia+

---
+++ Script/script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+  const html = document.documentElement;
+  const img = document.querySelector("#Perfil img");
+
+  html.classList.toggle("Light");
+
+  if (html.classList.contains("Light")) {
+    img.setAttribute("src", "./assets/avatar-light.png");
+  } else {
+    img.setAttribute("src", "./assets/avatar.png");
+  }
+}
