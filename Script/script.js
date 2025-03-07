e26d2af69aac1c8f95ff0975db7b94603e9a3ec0
Victor Fineto <victorfinetosilva@uol.com.br>
2025-03-07 16:59:39 -0300

    change Favicon by button

--- Script/script.js
+++ Script/script.js
@@ -1,15 +1,19 @@
 function toggleMode() {
   const html = document.documentElement
   const img = document.querySelector("#Perfil img")
+  const head = document.querySelector("#icon")
 
   if (html.classList.contains("Light")) {
     html.classList.remove("Light")
   } else {
     html.classList.add("Light")
   }
+
   if (html.classList.contains("Light")) {
     img.setAttribute("src", "Assets/avatar-light.png")
+    head.setAttribute("href", "Assets/avatar-light.png")
   } else {
     img.setAttribute("src", "Assets/avatar.png")
+    head.setAttribute("href", "Assets/avatar.png")
   }
 }
